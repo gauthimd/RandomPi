80cf9fa818d45f153d45f1245d609755bcec9307
Mike <gauthiermichael3.14@gmail.com>
2017-02-24 12:56:23 -0600

    Added optional line for dependencies

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #A short script for upgrading you RPi and getting your fortune
+#Uncomment the next line to download dependencies
+#sudo apt-get install -y sl toilet fortune cowsay lolcat
 sudo apt-get -y update   #Update
 sudo apt-get -y upgrade   #Upgrade
 sl-h -al  #Steam locomotive
